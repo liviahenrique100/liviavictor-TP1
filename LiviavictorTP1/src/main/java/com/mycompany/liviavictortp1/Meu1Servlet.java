b18cef8b451fa3414e1618103da02f3c2d0b75b3
victor <victor02065@gmail.com>
2019-10-24 11:58:33 -0200

    atividade 24/10 (real)

--- LiviavictorTP1/src/main/java/com/mycompany/liviavictortp1/Meu1Servlet.java
+++ LiviavictorTP1/src/main/java/com/mycompany/liviavictortp1/Meu1Servlet.java
@@ -29,19 +29,13 @@ public class Meu1Servlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet Meu1Servlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet Meu1Servlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
+      String nome= request.getParameter("nome");
+      String sobrenome= request.getParameter("sobrenome");
+      String nomeCompleto= nome+" "+sobrenome;
+      System.out.println ("nome completo: "+nomeCompleto);
+      response.sendRedirect ("teste.jsp"); 
+      //teste
+      
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
