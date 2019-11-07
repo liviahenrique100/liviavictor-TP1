49a081918d9df37e2010283184deb0c78fe92ea7
VictorEbaa <victor02065@gmail.com>
2019-11-07 11:42:18 -0200

    novos pacotes-2

--- LiviavictorTP1/src/main/java/com/mycompany/liviavictortp1/ultilidades/HibernateUtil.java
+++ LiviavictorTP1/src/main/java/com/mycompany/liviavictortp1/ultilidades/HibernateUtil.java
@@ -5,30 +5,14 @@
  */
 package com.mycompany.liviavictortp1.ultilidades;
 
-import entidades.Usuario;
+import com.mycompany.liviavictortp1.entidades.Usuario;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AnnotationConfiguration;
 
 public class HibernateUtil {
 
-//    private static final SessionFactory sessionFactory;
-//    static {
-//        try {
-//            sessionFactory = new AnnotationConfiguration()
-//                    .configure().buildSessionFactory();
-//        } catch (Throwable ex) {
-//            // Log exception!
-//            throw new ExceptionInInitializerError(ex);
-//        }
-//    }
-//
-//    public static Session getSession()
-//            throws HibernateException {
-//        return sessionFactory.openSession();
-//    }
-    
-    private static SessionFactory factory;
+    private static final SessionFactory factory;
 
     static {
         AnnotationConfiguration cfg = new AnnotationConfiguration();
