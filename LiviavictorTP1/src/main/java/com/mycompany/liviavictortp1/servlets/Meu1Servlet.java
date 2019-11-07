4e5339871c20a203e483af83fc884e74e81d700b
liviahenrique100 <liviahenrique100@gmail.com>
2019-11-07 10:46:04 -0200

    novos pacotes

--- LiviavictorTP1/src/main/java/com/mycompany/liviavictortp1/servlets/Meu1Servlet.java
+++ LiviavictorTP1/src/main/java/com/mycompany/liviavictortp1/servlets/Meu1Servlet.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.liviavictortp1;
+package com.mycompany.liviavictortp1.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
