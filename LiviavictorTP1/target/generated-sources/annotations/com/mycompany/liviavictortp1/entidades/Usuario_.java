f3ba4b6ea5b77733ba2660c2d563035950c4292c
liviahenrique100 <liviahenrique100@gmail.com>
2019-11-21 10:56:31 -0200

    21/11

---
+++ LiviavictorTP1/target/generated-sources/annotations/com/mycompany/liviavictortp1/entidades/Usuario_.java
@@ -0,0 +1,16 @@
+package com.mycompany.liviavictortp1.entidades;
+
+import java.math.BigDecimal;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-11-21T10:40:28")
+@StaticMetamodel(Usuario.class)
+public class Usuario_ { 
+
+    public static volatile SingularAttribute<Usuario, String> senha;
+    public static volatile SingularAttribute<Usuario, BigDecimal> idUsuario;
+    public static volatile SingularAttribute<Usuario, String> nome;
+
+}
